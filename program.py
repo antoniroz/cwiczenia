fc6f2d6d93834bd4afb23ed6fae074da856fe48c
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2020-06-06 12:13:02 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'cone.ui',
-# licensing of 'cone.ui' applies.
-#
-# Created: Sat Jun  6 08:57:20 2020
-#      by: pyside2-uic  running on PySide2 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
-
 import math
 from PySide2 import QtCore, QtGui, QtWidgets
 
@@ -18,8 +10,6 @@ class MyQWidget(QtWidgets.QWidget):
     
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
         
     def przelicz(self):
         h = self.ui.lineEdit_h.text()
@@ -27,7 +17,7 @@ class MyQWidget(QtWidgets.QWidget):
         
         h = float(h)
         d = float(d)
-        V = 1/3 * (d/2)**2 * h
+        V = 1/3 * (d/2)**3 * h
         V = str(V)        
         
         self.ui.lineEdit_V.setText(V)
